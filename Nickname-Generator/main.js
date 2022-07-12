e860e3eae80c1736b910feddbe30e91d1f56e410
leozheng <l.zheng444@gmail.com>
2022-07-12 08:28:51 -0600

    didnt hard code

--- Nickname-Generator/main.js
+++ Nickname-Generator/main.js
@@ -6,11 +6,6 @@ let allbutt = document.getElementById("allnick");
 let firstname = document.getElementById("firstname");
 let lastname = document.getElementById("lastname");
 let name = document.getElementById("name");
-let name1 = document.getElementById("name1");
-let name2 = document.getElementById("name2");
-let name3 = document.getElementById("name3");
-let name4 = document.getElementById("name4");
-let name5 = document.getElementById("name5");
 
 let words = ["Toe Curler", "the Coder", "the Sloth", "Mcchicken","the Parapelegic","the Potatoe"];
 // Add Event Listener to Calculate Button
@@ -19,20 +14,14 @@ randombutt.addEventListener("click", ranBtnClicked);
 
 function ranBtnClicked() {
   let ranlistnumb = Math.floor(Math.random() * 6);
-  name.innerHTML = firstname.value + ' ' +'"' + words[ranlistnumb] +'"' + ' ' + lastname.value;
-  name1.innerHTML = ' '
-  name2.innerHTML = ' '
-  name3.innerHTML = ' '
-  name4.innerHTML = ' '
-  name5.innerHTML = ' '
-}   
+  name.innerHTML = firstname.value + ' ' + '"' + words[ranlistnumb] + '"' + ' ' + lastname.value;
+}
 allbutt.addEventListener("click", allBtnClicked);
 
 function allBtnClicked() {
-  name.innerHTML = firstname.value + ' ' + '"' + words[0] +'"' + ' ' + lastname.value;
-  name1.innerHTML = firstname.value + ' ' + '"' +words[1] +'"' + ' ' + lastname.value;
-  name2.innerHTML = firstname.value + ' ' + '"' +words[2] +'"' + ' ' + lastname.value;
-  name3.innerHTML = firstname.value + ' ' + '"' +words[3] +'"' + ' ' + lastname.value;
-  name4.innerHTML = firstname.value + ' ' + '"' +words[4] +'"' + ' ' + lastname.value;
-  name5.innerHTML = firstname.value + ' ' + '"' +words[5] + '"' +' ' + lastname.value;
-}
+  let t = "";
+  for (let i = 0; i < 6; i++) {
+    t += firstname.value + ' ' + '"' + words[i] + '"' + ' ' + lastname.value + " <br> ";
+  }
+  name.innerHTML = t;
+ }
